3d8802452994e378d769bb3786deae782fb0d724
cdelorey <cdelorey@fake.com>
2013-03-16 18:45:24 -0300

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject clj-libtcod "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-native "0.9.3"]]
+  :main clj-libtcod.core
+  :source-paths ["src"])
+  
---
+++ test/clj_libtcod/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-libtcod.core-test
+  (:use clojure.test
+        clj-libtcod.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
