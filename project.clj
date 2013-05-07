fd1efa910ad52fb560b1cf6c12e1c2e950554cfa
cdelorey <cdelorey@fake.com>
2013-05-07 19:48:56 -0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject clj-libtcod "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-libtcod "0.1.0"
+  :description "Clojure bindings to the libtcod roguelike library."
+  :url "https://github.com/cdelorey/clj-libtcod"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-native "0.9.3"]]
   :main clj-libtcod.core
   :source-paths ["src"])
-  
